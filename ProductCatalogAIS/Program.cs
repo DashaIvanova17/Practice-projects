b59777cc38f0f47881a37c69a4139d9ab57ad459
DashaIvanova17 <144918443+DashaIvanova17@users.noreply.github.com>
2025-04-26 13:28:10 +0200

    Add files via upload

---
+++ ProductCatalogAIS/Program.cs
@@ -0,0 +1,21 @@
+﻿using ProductCatalogAIS.Views;
+using System;
+using System.Windows.Forms;
+
+namespace ProductCatalogAIS
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Инициализация базы данных перед запуском приложения
+            ProductCatalogAIS.Data.DatabaseHelper.InitializeDatabase();
+
+            Application.Run(new LoginForm());
+        }
+    }
+}
